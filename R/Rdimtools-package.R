191cacdaf0bd168adde3afc042af4a0711929fe0
kisungyou <ksyou496@gmail.com>
2018-09-26 20:09:49 -0400

    passed checks

--- R/Rdimtools-package.R
+++ R/Rdimtools-package.R
@@ -202,7 +202,7 @@
 #' @importFrom ADMM admm.lasso admm.spca admm.rpca
 #' @importFrom Matrix rankMatrix expm norm Matrix
 #' @importFrom Rtsne Rtsne
-#' @importFrom stats dist cov rnorm runif kmeans cor var sd approx lm coef coefficients as.dist hclust cutree quantile
+#' @importFrom stats dist cov rnorm runif kmeans cor var sd approx lm coef coefficients as.dist hclust cutree quantile median
 #' @importFrom graphics par image plot hist
 #' @importFrom Rcpp evalCpp
 #' @useDynLib Rdimtools
