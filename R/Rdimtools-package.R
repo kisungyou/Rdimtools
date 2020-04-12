a8ff2279643b197fe5822eb0281e6d407405f4aa
Kisung <ksyou496@gmail.com>
2020-04-12 15:50:55 -0400

    submission version

--- R/Rdimtools-package.R
+++ R/Rdimtools-package.R
@@ -221,7 +221,6 @@
 #' @importFrom utils packageVersion combn getFromNamespace
 #' @importFrom RSpectra eigs svds
 #' @importFrom ADMM admm.lasso admm.spca admm.rpca
-#' @importFrom Matrix rankMatrix expm norm Matrix
 #' @importFrom stats dist cov rnorm runif kmeans cor var sd approx lm coef coefficients as.dist hclust cutree quantile median integrate optimize
 #' @importFrom graphics par image plot hist
 #' @importFrom Rcpp evalCpp
